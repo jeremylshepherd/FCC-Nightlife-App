af90cbfc3e082454985f7f586a64014946f696c6
Jeremy Shepherd <jeremylshepherd@gmail.com>
2017-07-16 00:54:33 +0000

    Clean up Package,json heroku deploy

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log(`Node.js listening on port ${port}...`);
 });
